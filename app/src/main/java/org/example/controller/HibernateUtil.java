1a06ea4e21878e80ff9f704acd6f7571e0038d18
Riccardo <dlz.riccardo@gmail.com>
2025-10-22 23:42:07 +0200

    update hibernateUtile

--- app/src/main/java/org/example/controller/HibernateUtil.java
+++ app/src/main/java/org/example/controller/HibernateUtil.java
@@ -1,21 +1,30 @@
 package org.example.controller;
 
 
+import lombok.Getter;
+import org.hibernate.HibernateException;
 import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 
 public class HibernateUtil {
-    private static SessionFactory session; //= connect();
+    @Getter
+    private static final SessionFactory session = connect();
 
     private HibernateUtil() {}
 
-    public SessionFactory getSession() {
+    private static SessionFactory connect() {
+        try{
+            return new Configuration().configure().buildSessionFactory();
+        }catch (HibernateException e){
+            throw new ExceptionInInitializerError(e);
+        }
+    }
+
+    public static SessionFactory getSessionFactory() {
         return session;
     }
 
-    /*private static SessionFactory connect() {
-        try{
-
-
-        }
-    }*/
+    public static void close() {
+        getSession().close();
+    }
 }
