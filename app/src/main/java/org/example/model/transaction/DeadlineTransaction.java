bded5bc2366bc6d660b20c73dce41b6c58b5ae15
Riccardo <dlz.riccardo@gmail.com>
2025-10-22 17:25:30 +0200

    update  model component

--- app/src/main/java/org/example/model/transaction/DeadlineTransaction.java
+++ app/src/main/java/org/example/model/transaction/DeadlineTransaction.java
@@ -1,5 +1,15 @@
 package org.example.model.transaction;
 
-public class DeadlineTransaction extends Transaction {
 
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Entity
+@Getter
+@Setter
+public class DeadlineTransaction extends Transaction {
+    private Date deadline;
 }
--- app/src/main/java/org/example/model/transaction/Transaction.java
+++ app/src/main/java/org/example/model/transaction/Transaction.java
@@ -1,4 +1,24 @@
 package org.example.model.transaction;
 
+
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
+import org.example.model.tag.Tag;
+
+import java.util.Date;
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+
 public abstract class Transaction {
+    private int id;
+    private double amount;
+    private String description;
+    private Date date;
+    private Set<Tag> tag;
+
+
 }
