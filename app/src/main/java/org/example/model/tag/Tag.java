91404576a44d102fdad0d96b2413c72b686f680b
Riccardo <dlz.riccardo@gmail.com>
2025-10-22 17:25:30 +0200

    update  model component

--- app/src/main/java/org/example/model/tag/Tag.java
+++ app/src/main/java/org/example/model/tag/Tag.java
@@ -1,4 +1,15 @@
 package org.example.model.tag;
 
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+
 public class Tag {
+    private int id;
+    private String title;
+
 }
--- app/src/main/java/org/example/model/transaction/NormalTransaction.java
+++ app/src/main/java/org/example/model/transaction/NormalTransaction.java
@@ -1,4 +1,13 @@
 package org.example.model.transaction;
 
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
 public class NormalTransaction extends Transaction{
+
+
 }
