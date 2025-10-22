a79ae419f15f33d0ec2884a405f357c8af241f4c
Marco Pagnanini <marco.pagnanini2004@gmail.com>
2025-10-22 16:07:09 +0200

    Implement Hibernate

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,9 +17,9 @@ repositories {
 
 dependencies {
     implementation("org.hibernate.orm:hibernate-core:6.4.1.Final")
+    implementation("org.hibernate.orm:hibernate-community-dialects:6.4.1.Final")
     implementation("org.xerial:sqlite-jdbc:3.46.0.0")
     implementation("jakarta.persistence:jakarta.persistence-api:3.1.0")
-    implementation(kotlin("stdlib"))
 }
 
 // Apply a specific Java toolchain to ease working on different environments.
