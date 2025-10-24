b29b7b20050ffdcc47a12dce53f6545ff211aa32
Alberto <a.terenzi99@gmail.com>
2025-10-24 14:04:10 +0200

    add statistics

---
+++ app/src/main/java/org/example/model/statistics/Statistics.java
@@ -0,0 +1,14 @@
+package org.example.model.statistics;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Statistics {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+}
