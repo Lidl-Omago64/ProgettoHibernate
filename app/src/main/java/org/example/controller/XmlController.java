470f6706075c7143103588b57d7d09f059eb0672
Marco Pagnanini <marco.pagnanini2004@gmail.com>
2025-10-25 15:06:53 +0200

    Add XmlController

---
+++ app/src/main/java/org/example/controller/XmlController.java
@@ -0,0 +1,39 @@
+package org.example.controller;
+
+import org.w3c.dom.Document;
+
+import java.io.File;
+import java.util.List;
+
+public class XmlController<T> implements HibernateControllerInterface<T>{
+
+    private final File file;
+    private final Class<T> type;
+    private Document dom;
+
+
+    public XmlController(String path, Class<T> type){
+        this.file = new File(path);
+        this.type = type;
+    }
+
+    @Override
+    public void add(T t) {
+        return;
+    }
+
+    @Override
+    public void remove(Integer id) {
+        return;
+    }
+
+    @Override
+    public List<T> getAll() {
+        return List.of();
+    }
+
+    @Override
+    public T getById(Integer id) {
+        return null;
+    }
+}
